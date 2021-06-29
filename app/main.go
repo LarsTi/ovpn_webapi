42ba04e5321f68a15ab629daaadc2cfc30dc75fd
LarsTi <lars.tilsner@googlemail.com>
2021-06-29 16:10:20 +0200

    update

--- app/main.go
+++ app/main.go
@@ -2,11 +2,18 @@ package main
 
 import(
 	"log"
+	"os"
+
 )
 // User struct for my db
 func main(){
 	log.Println("Application startup")
-	log.Println("Please make sure, /docker/data is writable for execution user!")
+	log.Println("Please make sure, /docker/data, /docker/ccd and optional /docker/server is writable for execution user!")
+	log.Println("Checking for prototype Files")
+	checkFile("/data/server.ovpn.proto")
+	checkFile("/data/client.ovpn.proto")
+	log.Println("Finished check. Any Errors? It will not hardly fail, but i doubt it will work as intended")
+
 	db := connDB()
 	db.init()
 	ca := db.loadCA()
@@ -17,4 +24,12 @@ func main(){
 
 	//this one blocks!
 	RunWebApi(8080, db)
+}
+func checkFile(file string){
+	_, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		log.Printf("The file %s seems not to exist. Sure the program will work?!\n", file)
+		return
+	}
+	log.Printf("Found file %s. Assuming it is correct.\n", file)
 }
